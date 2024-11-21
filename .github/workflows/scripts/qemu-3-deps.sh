24cfa67207fa410d868bee0400a1f507666f2ac9
Tony Hutter <hutter2@llnl.gov>
2024-11-21 14:59:51 -0800

    Add Github runner to build zfs-release RPM (#93)
    Add a runner to build the zfs-release RPM and then package it
    up in artifacts.  This re-uses a stripped down version of
    the QEMU runners from ZFS:
    
    https://github.com/openzfs/zfs/commit/bca9b64e7b5a84916282a7f2506fbbc135b4a381
    
    Signed-off-by: Tony Hutter <hutter2@llnl.gov>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>

---
+++ .github/workflows/scripts/qemu-3-deps.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+######################################################################
+# 3) install dependencies for compiling and loading
+######################################################################
+
+set -eu
+
+sudo dnf install rpm-tools
+
+exit 0
